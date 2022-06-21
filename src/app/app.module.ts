725f7b1d760473ef5159978e07925920bf20f86e
Harsh Sharma <harshsharma56384@gmail.com>
2022-06-21 11:33:44 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BusinessLoanComponent } from './business-loan/business-loan.component';
+import { PersonalLoanComponentComponent } from './personal-loan-component/personal-loan-component.component';
+import { HouseLoanComponentComponent } from './house-loan-component/house-loan-component.component';
+import { DefaultcomponentComponent } from './defaultcomponent/defaultcomponent.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BusinessLoanComponent,
+    PersonalLoanComponentComponent,
+    HouseLoanComponentComponent,
+    DefaultcomponentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
